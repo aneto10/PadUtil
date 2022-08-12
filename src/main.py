d1098fb6057d752cea9fa8533b45f7e383801545
kaique64 <kaiquehenrique.2005@outlook.com>
2022-08-12 14:35:49 -0300

    get filepath and tag from console

--- src/main.py
+++ src/main.py
@@ -5,7 +5,10 @@ sys.path.append(r"C:/Users/tc.kcosta/PadUtil/src")
 from services.inventory_service.inventory_service_xls import InventoryServiceXLS
 from controllers.inventory.inventory_controller import InventoryController
 
-inventoryService = InventoryServiceXLS("FILEPATH", "TAG")
+filePath: str = input('Insira o caminho do arquivo: ')
+tag: str = input('Insira a tag: ')
+
+inventoryService = InventoryServiceXLS(filePath, tag)
 
 controller = InventoryController(inventoryService)
 controller.read_inventory()
